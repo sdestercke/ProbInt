0b2701cf816d7ed518a36b91aa48f5a5b47913d4
Sebastien Destercke <sdestercke@gmail.com>
2012-02-23 18:35:33 +0100

    started a class to handle sets of probability intervals

--- intervalsProbability.py
+++ intervalsProbability.py
@@ -1,20 +1,23 @@
 import numpy as np
 
 class intervalsProbability:
-    """Intervalprobability class
+    """Class of probability intervals: upper and lower prob. bounds on singletons
 
     Argument:
     lproba -- a 2xn array containing upper (1st row) and lower (2nd row) bounds
     
     """
+    
     def __init__(self,lproba):
-        if lproba.__class__.__name__!='ndarray':
+        if lproba.__class__.__name__ != 'ndarray':
             raise Exception('Expecting a numpy array as argument')
-        if np.size(lproba[:,1])!=2:
+        if lproba[:,1].size != 2:
             raise Exception('Array should contain two rows: top for upper prob, bottom for lower prob')
+        if lproba.ndim != 2:
+            raise Exception('Bad dimension of array: should contain 2 dimensions')
         self.lproba=lproba
-        self.nbDecision=np.size(lproba[0])
-        if np.all(lproba[0]>lproba[1])!=1:
+        self.nbDecision=lproba[0].size
+        if np.all(lproba[0] >=lproba[1]) != 1:
             raise Exception('Some upper bounds lower than lower bounds')
 
     def isProper(self):
@@ -23,7 +26,7 @@ class intervalsProbability:
         Return 0 (empty) or 1 (non-empty).
         
         """
-        if np.sum(self.lproba[1,:])<=1 and np.sum(self.lproba[0,:])>=1:
+        if self.lproba[1,:].sum()<=1 and self.lproba[0,:].sum()>=1:
             return 1
         else:
             return 0
@@ -39,32 +42,32 @@ class intervalsProbability:
         """
         if subset.__class__.__name__!='ndarray':
             raise Exception('Expecting a numpy array as argument')
-        if np.size(subset)!=self.nbDecision:
+        if subset.size != self.nbDecision:
             raise Exception('Subset incompatible with the frame size')
         if self.isReachable()==0:
             self.setReachableProbability()
         else:
-            lowerProbability=max(np.sum(self.lproba[1,subset[:]==1]),1-np.sum(self.lproba[0,subset[:]==0]))
+            lowerProbability=max(self.lproba[1,subset[:]==1].sum(),1-self.lproba[0,subset[:]==0].sum())
             return lowerProbability
 
     def getUpperProbability(self,subset):
         if subset.__class__.__name__!='ndarray':
             raise Exception('Expecting a numpy array as argument')
-        if np.size(subset)!=self.nbDecision:
+        if subset.size != self.nbDecision:
             raise Exception('Subset incompatible with the frame size')
         if self.isReachable()==0:
             self.setReachableProbability()
         else:
-            upperProbability=min(np.sum(self.lproba[0,subset[:]==1]),1-np.sum(self.lproba[1,subset[:]==0]))
+            upperProbability=min(self.lproba[0,subset[:]==1].sum(),1-self.lproba[1,subset[:]==0].sum())
             return upperProbability
 
     def isReachable(self):
         for i in range(self.nbDecision):
             subset=np.ones(self.nbDecision)
             subset[i]=0
-            if self.lproba[0,i] + np.sum(self.lproba[1,subset[:]==1])  > 1.0:
+            if self.lproba[0,i] + self.lproba[1,subset[:]==1].sum()  > 1.0:
                 return 0
-            if self.lproba[1,i] + np.sum(self.lproba[0,subset[:]==1]) < 1.0:
+            if self.lproba[1,i] + self.lproba[0,subset[:]==1].sum() < 1.0:
                 return 0
         return 1
 
@@ -74,8 +77,8 @@ class intervalsProbability:
             for i in range(self.nbDecision):
                 subset=np.ones(self.nbDecision)
                 subset[i]=0
-                lb=max(self.lproba[1,i],1-np.sum(self.lproba[0,subset[:]==1]))
-                ub=min(self.lproba[0,i],1-np.sum(self.lproba[1,subset[:]==1]))
+                lb=max(self.lproba[1,i],1-self.lproba[0,subset[:]==1].sum())
+                ub=min(self.lproba[0,i],1-self.lproba[1,subset[:]==1].sum())
                 lreachableProba[1,i]=lb
                 lreachableProba[0,i]=ub
             self.lproba[:]=lreachableProba[:]
@@ -95,7 +98,69 @@ class intervalsProbability:
         print " "*11, "-"*20
         print str1
         print str2
+        
+class setOfIntProba:
+    """Class to handle sets of Int Proba
 
+    Argument:
+    intlist -- a mx2xn array containing upper (1st row) and lower (2nd row) bounds
+               of the m probability intervals
+               
+               dim 1: index of probability set
+               dim 2: lower or upper prob bounds
+               dim 3: values of bounds on each element
+    """
+    
+    def __init__(self,intlist):
+        if intlist.__class__.__name__ != 'ndarray':
+            raise Exception('Expecting a numpy array as argument')
+        if intlist.ndim != 3:
+            raise Exception('Expecting a 3-dimensional array')
+        self.intlist=intlist
+        self.nbProbInt=intlist[:,0,0].size
+        self.nbDecision=intlist[0,0].size
+        
+    def areCompatible(self):
+        """Check whether the set of probability intervals are compatible, i.e., if the conjunction is non-empty
+        """
+        min=self.intlist[:,1,:].max(axis=0)
+        max=self.intlist[:,0,:].min(axis=0)
+        if min.sum() <= 1 and max.sum() >= 1:
+            return 1
+        else:
+            return 0
+            
+    def conjunction(self):
+        """Perform a conjunctive merging of the set of probability intervals
+        
+        Return a possibly non-proper intervalsProbability class object.
+        """
+        fusedproba=np.zeros((2,self.nbDecision))
+        for i in range(self.nbDecision):
+            subset=np.ones(self.nbDecision)
+            subset[i]=0
+            lb=max(self.intlist[:,1,i].max(),1-self.intlist[:,0,subset[:]==1].min(axis=0).sum())
+            ub=min(self.intlist[:,0,i].min(),1-self.intlist[:,1,subset[:]==1].max(axis=0).sum())
+            fusedproba[1,i]=lb
+            fusedproba[0,i]=ub
+        result=intervalsProbability(fusedproba)
+        return result
+        
+    def disjunction(self):
+        """Perform a disjunctive merging of the set of probability intervals
+        
+        Return an intervalsProbability class object.
+        """
+        fusedproba=np.zeros((2,self.nbDecision))
+        for i in range(self.nbDecision):
+            subset=np.ones(self.nbDecision)
+            subset[i]=0
+            lb=self.intlist[:,1,i].min()
+            ub=self.intlist[:,0,i].max()
+            fusedproba[1,i]=lb
+            fusedproba[0,i]=ub
+        result=intervalsProbability(fusedproba)
+        return result        
 
 if __name__=='__main__':
     lproba =np.array([[0.4,0.4,0.5],[0.2,0,0.2]])
@@ -104,5 +169,12 @@ if __name__=='__main__':
     print essai.getUpperProbability(np.array([1,1,1]))
     essai.printProbability()
     essai.setReachableProbability()
-    print essai.getUpperProbability([1,0,1]) #should give an error.
+    print essai.getUpperProbability(np.array[1,0,1])
     essai.printProbability()
+    
+    setproba=np.array([[[0.6,0.4,0.5],[0.2,0.3,0.2]],[[0.7,0.3,0.6],[0.2,0.,0.2]],
+                    [[0.7,0.3,0.6],[0.2,0.,0.2]],[[0.6,0.4,0.5],[0.2,0.3,0.2]]])
+    test=Intprob.setOfIntProba(setproba)
+    test.areCompatible()
+    test.conjunction()
+
