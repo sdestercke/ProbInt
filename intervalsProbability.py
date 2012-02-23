3b866290d1dd7efb87d150b871be3fe29c095e0b
Sebastien Destercke <sdestercke@gmail.com>
2012-02-23 14:31:43 +0100

    New version using numpy, arrays and related functions

--- intervalsProbability.py
+++ intervalsProbability.py
@@ -3,60 +3,69 @@ import numpy as np
 class intervalsProbability:
 
     def __init__(self,lproba):
+        if lproba.__class__.__name__!='ndarray':
+            raise Exception('Expecting a numpy array as argument')
+        if np.size(lproba[:,1])!=2:
+            raise Exception('Array should contain two rows: top for upper prob, bottom for lower prob')
         self.lproba=lproba
-        self.nbDecision=len(lproba)
+        self.nbDecision=np.size(lproba[0])
+        if np.all(lproba[0]>lproba[1])!=1:
+            raise Exception('Some upper bounds lower than lower bounds')
 
     def isProper(self):
-        sumLowerBound=0
-        sumUpperBound=0
-        for interval in self.lproba:
-            sumLowerBound+=interval[0]
-            sumUpperBound+=interval[1]
-        if sumLowerBound<=1 and sumUpperBound>=1:
+        if np.sum(self.lproba[1,:])<=1 and np.sum(self.lproba[0,:])>=1:
             return 1
         else:
             return 0
 
     def getLowerProbability(self,subset):
-        if len(subset)!=self.nbDecision:
-            raise Exception('Subset incompatible with the probabilities')
+        if subset.__class__.__name__!='ndarray':
+            raise Exception('Expecting a numpy array as argument')
+        if np.size(subset)!=self.nbDecision:
+            raise Exception('Subset incompatible with the frame size')
         else:
-            lowerProbability=0
-            for i in range(self.nbDecision):
-                if subset[i]!=0:
-                    lowerProbability+=self.lproba[i][0]
+            lowerProbability=max(np.sum(self.lproba[1,subset[:]==1]),1-np.sum(self.lproba[0,subset[:]==0]))
             return lowerProbability
 
     def getUpperProbability(self,subset):
-        if len(subset)!=self.nbDecision:
-            raise Exception('Subset incompatible with the probabilities')
+        if subset.__class__.__name__!='ndarray':
+            raise Exception('Expecting a numpy array as argument')
+        if np.size(subset)!=self.nbDecision:
+            raise Exception('Subset incompatible with the frame size')
         else:
-            upperProbability=0
-            for i in range(self.nbDecision):
-                if subset[i]!=0:
-                    upperProbability+=self.lproba[i][1]
+            upperProbability=min(np.sum(self.lproba[0,subset[:]==1]),1-np.sum(self.lproba[1,subset[:]==0]))
             return upperProbability
 
+    def isReachable(self):
+        for i in range(self.nbDecision):
+            subset=np.ones(self.nbDecision)
+            subset[i]=0
+            if self.lproba[0,i] + np.sum(self.lproba[1,subset[:]==1])  > 1.0:
+                return 0
+            if self.lproba[1,i] + np.sum(self.lproba[0,subset[:]==1]) < 1.0:
+                return 0
+        return 1
 
     def setReachableProbability(self):
         if self.isProper()==1:
-            self.lreachableProba=[]
-            for i in range(0,self.nbDecision):
+            lreachableProba=np.zeros((2,self.nbDecision))
+            for i in range(self.nbDecision):
                 subset=np.ones(self.nbDecision)
                 subset[i]=0
-                lb=max(lproba[i][0],1-self.getUpperProbability(subset))
-                ub=min(lproba[i][1],1-self.getLowerProbability(subset))
-                self.lreachableProba.append([lb,ub])
-            self.lproba=self.lreachableProba
+                lb=max(lproba[1,i],1-self.getUpperProbability(subset))
+                ub=min(lproba[0,i],1-self.getLowerProbability(subset))
+                lreachableProba[1,i]=lb
+                lreachableProba[0,i]=ub
+            self.lproba[:]=lreachableProba[:]
 
     def printProbability(self):
-        str1,str2="lower bound |","upper bound |"
+        str1,str2="upper bound |","lower bound |"
         str3=" "*13;
         i=0
-        for interval in self.lproba:
+        for interval in range(self.nbDecision):
             str3+="   y%d "%i
-            str1+=" %.3f" % interval[0]
-            str2+=" %.3f" % interval[1]
+            str1+=" %.3f" % self.lproba[0,interval]
+            str2+=" %.3f" % self.lproba[1,interval]
             i+=1
         print str3
         print " "*11, "-"*20
@@ -65,11 +74,11 @@ class intervalsProbability:
 
 
 if __name__=='__main__':
-    #lproba=[[0.1,0.2],[0.3,0.7],[0.5,0.9]]
-    lproba=[[0.3,0.7],[0.5,0.9]]
+    lproba =np.array([[0.4,0.4,0.5],[0.2,0,0.2]])
     essai=intervalsProbability(lproba)
     print essai.isProper()
-    print essai.getUpperProbability([1,1])
+    print essai.getUpperProbability(np.array([1,1,1]))
     essai.printProbability()
     essai.setReachableProbability()
+    print essai.getUpperProbability([1,0,1])
     essai.printProbability()
